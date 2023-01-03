d16cdcb408110df3b0061322280ac25dc4dc455f
Kunniii <nguyenairport56@gmail.com>
2023-01-03 08:00:24 +0700

    fetch xml and display number of post

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ function App() {
   }, []);
 
   return (
-    <div className="w-[60em] text-center">
-      <div className="rounded bg-red-500">
-        <p className="text-3xl font-bold text-white px-5 py-2">{postCount}</p>
+    <div className="text-center mx-auto mt-5 w-[75vw] h-[30vh]">
+      <div className="rounded bg-red-500 mx-auto">
+        <p className="text-xl font-bold text-white">{postCount}</p>
       </div>
     </div>
   );
